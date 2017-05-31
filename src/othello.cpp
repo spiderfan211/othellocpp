f5176539364634eac6cc270d3c721fa20f50f363
spiderfan211 <spiderfan211@gmail.com>
2017-05-31 14:47:44 +0200

    mi othello por ahora

--- src/othello.cpp
+++ src/othello.cpp
@@ -1,9 +1,41 @@
 #include<iostream>
+#include<string>
 #include "Jugador.h"
 
 using namespace std;
 
 int main(){
+  string seguir_jugando = "¿Seguir jugando? (S/N)";
+  string msg_error = "Debes introducir un valor correcto";
+  Tablero t;
+  Jugador J1, J2;
+  bool seguir = true, error = false;
+  char c;
+  while(seguir){
+    do {
 
 
+
+
+
+
+
+
+
+
+    }while (t.IsFinalizada() == false);
+    do{
+      cout << seguir_jugando << endl;
+      cin >> c;
+      if ( c == 's' || c == 'S' )
+        seguir = true;
+        error = false;
+      else if ( c == 'n' || c == 'N' )
+        seguir = false;
+        error = false;
+      else
+        error = true;
+        cout << msg_error << endl;
+    } while (error)
+  }
 }
